d59eb7ba6f046ddac4a5377920860ed102e4edb1
Breno Cunha Queiroz <breno1423@gmail.com>
2023-09-14 07:46:25 +0200

    Feat: Encoder CRC check

--- firmware/src/drivers/encoder/encoder.cpp
+++ firmware/src/drivers/encoder/encoder.cpp
@@ -17,7 +17,30 @@ bool Encoder::init() {
     return true;
 }
 
+static uint8_t calculateCRC(uint32_t data) {
+    // Thanks to https://github.com/scottbez1/smartknob/blob/master/firmware/src/mt6701_sensor.cpp
+    static uint8_t tableCRC6[64] = {0x00, 0x03, 0x06, 0x05, 0x0C, 0x0F, 0x0A, 0x09, 0x18, 0x1B, 0x1E, 0x1D, 0x14, 0x17, 0x12, 0x11,
+                                    0x30, 0x33, 0x36, 0x35, 0x3C, 0x3F, 0x3A, 0x39, 0x28, 0x2B, 0x2E, 0x2D, 0x24, 0x27, 0x22, 0x21,
+                                    0x23, 0x20, 0x25, 0x26, 0x2F, 0x2C, 0x29, 0x2A, 0x3B, 0x38, 0x3D, 0x3E, 0x37, 0x34, 0x31, 0x32,
+                                    0x13, 0x10, 0x15, 0x16, 0x1F, 0x1C, 0x19, 0x1A, 0x0B, 0x08, 0x0D, 0x0E, 0x07, 0x04, 0x01, 0x02};
+    uint8_t idx = 0;
+    uint8_t crc = 0;
+
+    idx = (uint8_t)(((uint32_t)data >> 12u) & 0x0000003Fu);
+
+    crc = (uint8_t)(((uint32_t)data >> 6u) & 0x0000003Fu);
+    idx = crc ^ tableCRC6[idx];
+
+    crc = (uint8_t)((uint32_t)data & 0x0000003Fu);
+    idx = crc ^ tableCRC6[idx];
+
+    crc = tableCRC6[idx];
+
+    return crc;
+}
+
 float Encoder::readAngle() {
+    // TODO Use 4MHz SPI
     uint16_t data = 0;
     uint8_t status = 0;
     uint8_t crc = 0;
@@ -52,11 +75,25 @@ float Encoder::readAngle() {
     }
     Gpio::write(Gpio::ENC_CSN_PIN, true);
 
-    float angle = data / 16384.0f;
+    // Calculate angle
+    float angle = data / 16384.0f * 3.141592f * 2.0f; // Calculate angle in radians
 
-    Uart::transmit("  - Data: " + std::to_string(data) + "\n");
-    Uart::transmit("  - Status: " + std::to_string((int)status) + "\n");
-    Uart::transmit("  - CRC: " + std::to_string((int)crc) + "\n");
+    // Check status
+    if (status & STATUS_MAG_FIELD_TOO_STRONG)
+        Uart::transmit("Status: MAG_FIELD_TOO_STRONG\n");
+    if (status & STATUS_MAG_FIELD_TOO_WEAK)
+        Uart::transmit("Status: MAG_FIELD_TOO_WEAK\n");
+    if (status & STATUS_PUSH_BUTTON_DETECTED)
+        Uart::transmit("Status: PUSH_BUTTON_DETECTED\n");
+    if (status & STATUS_LOSS_OF_TRACK)
+        Uart::transmit("Status: LOSS_OF_TRACK\n");
 
-    return angle;
+    // Check CRC
+    uint8_t crcReceived = calculateCRC(uint32_t(data) << 4 | status);
+    if (crcReceived == crc)
+        return angle;
+
+    // CRC error
+    Uart::transmit("CRC mismatch: " + std::to_string(crcReceived) + " instead of " + std::to_string(crc));
+    return -1.0f;
 }
--- firmware/src/drivers/encoder/encoder.h
+++ firmware/src/drivers/encoder/encoder.h
@@ -12,6 +12,16 @@ class Encoder {
     bool init();
 
     float readAngle();
+
+  private:
+    // clang-format off
+    enum Status {
+        STATUS_MAG_FIELD_TOO_STRONG = 0b0001,
+        STATUS_MAG_FIELD_TOO_WEAK   = 0b0010,
+        STATUS_PUSH_BUTTON_DETECTED = 0b0100,
+        STATUS_LOSS_OF_TRACK        = 0b1000
+    };
+    // clang-format on
 };
 
 inline Encoder encoder;
